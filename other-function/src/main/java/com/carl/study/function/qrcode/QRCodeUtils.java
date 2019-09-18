0988402382421d4b54069e015be02fbd430ec078
changerzhuo <cez@prowizdom.com>
2019-09-18 08:34:14 +0800

    生成二维码, 解决二维码中文内容乱码问题

--- other-function/src/main/java/com/carl/study/function/qrcode/QRCodeUtils.java
+++ other-function/src/main/java/com/carl/study/function/qrcode/QRCodeUtils.java
@@ -2,6 +2,7 @@ package com.carl.study.function.qrcode;
 
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
@@ -54,7 +55,9 @@ public class QRCodeUtils {
         log.info("生成二维码内容={}, width={}, height={},filePath={}", text, width, height, filePath);
         try {
             QRCodeWriter qrCodeWriter = new QRCodeWriter();
-            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+            Map<EncodeHintType, String> encodeHintTypeStringMap = new HashMap<>();
+            encodeHintTypeStringMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, encodeHintTypeStringMap);
             Path path = FileSystems.getDefault().getPath(filePath);
             MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
         } catch (Exception e) {
